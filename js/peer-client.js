2b3a55905bc71b1e830583bf3da808a169a0cf96
Devill Don <53294521+devillD@users.noreply.github.com>
2021-12-14 15:52:47 +0600

    Update peer-client.js

--- js/peer-client.js
+++ js/peer-client.js
@@ -3,7 +3,7 @@ peerapp = (function() {
 
     console.log("Peer client started");
 
-    var PEER_SERVER = 'peerjs-abirxo.cloud.okteto.net';
+    var PEER_SERVER = 'my-peer.herokuapp.com';
     var PORT = 443;
     var connectedPeers = {};
     var myPeerID = generateRandomID(4);
